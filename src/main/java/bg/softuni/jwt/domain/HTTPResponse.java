83fa49eb3abdc3a20e52045af7a7c85b48c03831
rborisovv <radii2000@abv.bg>
2022-08-05 21:23:11 +0300

    Added JWT Impl and Serializable interface to entities

--- src/main/java/bg/softuni/jwt/domain/HTTPResponse.java
+++ src/main/java/bg/softuni/jwt/domain/HTTPResponse.java
@@ -1,13 +1,18 @@
 package bg.softuni.jwt.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
-public class HTTPResponse {
+public class HTTPResponse implements Serializable {
     private Integer httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;

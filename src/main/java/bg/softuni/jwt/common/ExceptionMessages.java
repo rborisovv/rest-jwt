342fbd258d8a86459187a657e208a8fbc19c19ca
rborisovv <radii2000@abv.bg>
2022-08-07 21:43:38 +0300

    Added white label error page implementation

--- src/main/java/bg/softuni/jwt/common/ExceptionMessages.java
+++ src/main/java/bg/softuni/jwt/common/ExceptionMessages.java
@@ -8,4 +8,5 @@ public class ExceptionMessages {
     public static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
     public static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
     public static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
+    public static final String PAGE_NOT_FOUND = "This page was not found!";
 }
--- src/main/java/bg/softuni/jwt/exception/ExceptionHandler.java
+++ src/main/java/bg/softuni/jwt/exception/ExceptionHandler.java
@@ -1,5 +1,6 @@
 package bg.softuni.jwt.exception;
 
+import bg.softuni.jwt.common.ExceptionMessages;
 import bg.softuni.jwt.domain.HTTPResponse;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.persistence.NoResultException;
 import javax.security.auth.login.AccountLockedException;
@@ -20,6 +22,8 @@ import java.util.Locale;
 import java.util.Objects;
 
 import static bg.softuni.jwt.common.ExceptionMessages.*;
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice
@@ -27,74 +31,78 @@ public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(DisabledException.class)
     public ResponseEntity<HTTPResponse> accountDisabledException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+        return createHttpResponse(BAD_REQUEST, ACCOUNT_DISABLED);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<HTTPResponse> badCredentialsException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, INCORRECT_CREDENTIALS);
+        return createHttpResponse(BAD_REQUEST, INCORRECT_CREDENTIALS);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<HTTPResponse> accessDeniedException() {
-        return createHttpResponse(HttpStatus.FORBIDDEN, NOT_ENOUGH_PERMISSION);
+        return createHttpResponse(FORBIDDEN, NOT_ENOUGH_PERMISSION);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(AccountLockedException.class)
     public ResponseEntity<HTTPResponse> accountLockedException() {
-        return createHttpResponse(HttpStatus.UNAUTHORIZED, ACCOUNT_LOCKED);
+        return createHttpResponse(UNAUTHORIZED, ACCOUNT_LOCKED);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(TokenExpiredException.class)
     public ResponseEntity<HTTPResponse> tokenExpiredException(TokenExpiredException exception) {
-        return createHttpResponse(HttpStatus.UNAUTHORIZED, exception.getMessage().toUpperCase(Locale.ROOT));
+        return createHttpResponse(UNAUTHORIZED, exception.getMessage().toUpperCase(Locale.ROOT));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(EmailExistsException.class)
     public ResponseEntity<HTTPResponse> emailExistsException(EmailExistsException exception) {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(EmailNotFoundException.class)
     public ResponseEntity<HTTPResponse> emailNotFoundException(EmailNotFoundException exception) {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(UsernameExistsException.class)
     public ResponseEntity<HTTPResponse> usernameExistsException(UsernameNotFoundException exception) {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<HTTPResponse> userNotFoundException(UserNotFoundException exception) {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, exception.getMessage());
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HTTPResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
         HttpMethod supportedMethod = Objects.requireNonNull(exception.getSupportedHttpMethods()).iterator().next();
-        return createHttpResponse(HttpStatus.METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportedMethod));
+        return createHttpResponse(METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportedMethod));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(Exception.class)
     public ResponseEntity<HTTPResponse> interServerErrorException(Exception exception) {
         log.error(exception.getMessage());
-        return createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR);
+        return createHttpResponse(INTERNAL_SERVER_ERROR, ExceptionMessages.INTERNAL_SERVER_ERROR);
     }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<HTTPResponse> whiteLabelErrorException() {
+        return createHttpResponse(BAD_REQUEST, PAGE_NOT_FOUND);
+    }
+
 
     @org.springframework.web.bind.annotation.ExceptionHandler(NoResultException.class)
     public ResponseEntity<HTTPResponse> notFoundException(NoResultException exception) {
         log.error(exception.getMessage());
-        return createHttpResponse(HttpStatus.NOT_FOUND, exception.getMessage());
+        return createHttpResponse(NOT_FOUND, exception.getMessage());
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(IOException.class)
     public ResponseEntity<HTTPResponse> ioException(IOException exception) {
         log.error(exception.getMessage());
-        return createHttpResponse(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_PROCESSING_FILE);
+        return createHttpResponse(INTERNAL_SERVER_ERROR, ERROR_PROCESSING_FILE);
     }
-
-
 
     private ResponseEntity<HTTPResponse> createHttpResponse(HttpStatus httpStatus, String message) {
         HTTPResponse httpResponse = new HTTPResponse(httpStatus.value(),
